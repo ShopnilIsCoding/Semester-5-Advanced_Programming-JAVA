eb2c85168dedfe04aef99bd726e734bbffa1e6cc
ShopnilIsCoding <shopnilkhan766@gmail.com>
2024-09-04 15:06:54 +0600

    fixed the row counts

--- lab_2/DiamondPattern.java
+++ lab_2/DiamondPattern.java
@@ -1,32 +1,39 @@
 import java.util.Scanner;
 
-public class StarDiamondPattern {
+public class DiamondPattern {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number of rows for the diamond: ");
-        int rows = scanner.nextInt();
-        int n = rows / 2;
-
-        // Upper 
+        
+        // Asking the user to enter the number of rows for the diamond
+        System.out.print("Enter number of rows: ");
+        int n = scanner.nextInt();
+        
+        // Upper half of the diamond
         for (int i = 1; i <= n; i++) {
-            for (int j = i; j <= n; j++) {
+            // Printing spaces
+            for (int j = i; j < n; j++) {
                 System.out.print(" ");
             }
+            // Printing stars
             for (int j = 1; j <= (2 * i - 1); j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
 
-        // Lower 
+        // Lower half of the diamond
         for (int i = n - 1; i >= 1; i--) {
-            for (int j = n; j >= i; j--) {
+            // Printing spaces
+            for (int j = n; j > i; j--) {
                 System.out.print(" ");
             }
+            // Printing stars
             for (int j = 1; j <= (2 * i - 1); j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
+
+        scanner.close();
     }
 }
